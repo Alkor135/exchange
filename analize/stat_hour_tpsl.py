8dbc519167c899ca155169e0b019d2562424fcc6
Aleksey Manin <Alkor135@gmail.com>
2022-02-15 12:15:09 +0300

    stat

--- analize/stat_hour_tpsl.py
+++ analize/stat_hour_tpsl.py
@@ -1,6 +1,7 @@
 """
 Скрипт собирает статистику по файлам ТПСЛ
 """
+from typing import Any
 import re
 from pathlib import *
 from datetime import datetime
@@ -12,14 +13,15 @@ pd.set_option('max_rows', 5)  # Установка 5 строк вывода DF
 pd.set_option('display.max_columns', None)  # Сброс ограничений на число столбцов
 
 
-def zero_hour(cell):
+def zero_hour(cell: Any) -> str:
     """ Функция преобразует время (с финама приходят часы без нулей (с марта 2021), которые pandas не воспринимает)"""
-    cell = f'{int(cell)}'
-    tmp_time = datetime.strptime(cell, "%H%M%S")
+    cell: str = f'{int(cell)}'
+    tmp_time: datetime = datetime.strptime(cell, "%H%M%S")
     return tmp_time.strftime("%H%M%S")
 
 
-def dataframe_datetime_prepare(df: pd):
+def dataframe_datetime_prepare(df: pd) -> pd:
+    """ Функция обрабатывает столбцы с датой и временем"""
     df['<TIME>'] = df.apply(lambda x: zero_hour(x['<TIME>']),
                             axis=1)  # Преобразуем столбец <TIME>, где нужно добавив 0 перед часом
     df['<DATE_TIME>'] = df['<DATE>'].astype(str) + ' ' + df['<TIME>'].astype(
@@ -33,28 +35,21 @@ def dataframe_datetime_prepare(df: pd):
 def run(files_path, razmer, year):
     result: pd = pd.DataFrame()
     for ind_file, file in enumerate(files_path, start=1):  # Итерация по тиковым файлам
+        # result: pd = pd.DataFrame()
         df = pd.read_csv(file, delimiter=',')  # Считываем данные в DF
         df = dataframe_datetime_prepare(df)
-        # print(df)
+
         for row in df.itertuples():
-            if row[0].hour in result.columns:
-                print('first')
-                if row[7] == 3:
-                    print('second')
-                    result.loc[0, row[0].hour] = 1
-            else:
-                # result.insert(0, row[0].hour, 0)
-                result[row[0].hour] = 0
+            if row[0].hour not in result.columns:  # Если столбец с таким часом не существует
+                result[row[0].hour] = [0, 0]  # Создаем столбец, заполняем нулями
 
-            # print(row[0])
-            # print(row[0].hour)
-            # tmp_time = datetime.strftime(row[0], "%Y-%m-%d %H:%M:%S")
-            # print(tmp_time)
-            # # print(datetime.strptime(row[2]))
-            # # print(row[2])
-            # break
+            if row[7] == 3:
+                result.loc[0, row[0].hour] += 3  #
+            elif row[7] == -1:
+                result.loc[0, row[0].hour] -= 1
+        # print(result)
+        # print()
 
-        break
     print(result)
 
 
