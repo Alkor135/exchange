2d16ab5f1faa62bab2419ab7eb1d631bd5c55ef3
Aleksey Manin <Alkor135@gmail.com>
2022-02-28 16:41:46 +0300

    stat

--- chart_finplot/chart_mvc_tpsl_01.py
+++ chart_finplot/chart_mvc_tpsl_01.py
@@ -1,6 +1,7 @@
 """
 Cкрипт отрисовки графиков с макс volume cluster и TP/SL
 """
+from typing import Any
 from pathlib import *
 from datetime import datetime, timezone
 
@@ -27,38 +28,44 @@ def plot_tpsl(df, ax):
     df['<TP_SL>'].plot(ax=ax, kind='bar', legend='TP_SL')  #
 
 
-def plot_ema(df, ax):
+def plot_vol(df, ax):
+    """ Отрисовка ТП/СЛ """
+    df['<VOL>'].plot(ax=ax, kind='bar', legend='VOLUME')  #
+
+
+def plot_ema(df, ax, period):
     """Отрисовка индикатора ЕМА"""
-    df['<CLOSE>'].ewm(span=18).mean().plot(ax=ax, legend='EMA')
+    df['<CLOSE>'].ewm(span=period).mean().plot(ax=ax, legend=f'EMA {period}')
 
 
-def edge_prepare(df, edge_only):
-    col_df = list(df.columns.values)  # Названия колонок в список
-    high_idx = col_df.index('<HIGH>') + 1  # Получение индекса колонки
-    low_idx = col_df.index('<LOW>') + 1  # Получение индекса колонки
-    mvc_idx = col_df.index('<MAX_VOLUME_PRICE>') + 1  # Получение индекса колонки
-    close_idx = col_df.index('<CLOSE>') + 1  # Получение индекса колонки
+def edge_prepare(df: pd, edge_only: Any):
+    """ Функция меняет значения колонки TP_SL, если не удовлетворяет перцентилю записывается 0"""
+    def replace_cell(high: float, low: float, close: float, mvc: float, tpsl: int) -> int:
+        df_tmp = pd.DataFrame({'field': [low, high]})
+        if low <= mvc <= high:  # Если макс объем в баре (защита от ошибочных баров)
+            if close == high:  # Если бар на повышение
+                val = df_tmp['field'].quantile(edge_only)  # Перцентиль
+                if mvc <= val:
+                    return tpsl
+            elif close == low:  # Если бар на понижение
+                val = df_tmp['field'].quantile(1 - edge_only)  # Перцентиль
+                if mvc >= val:
+                    return tpsl
+        return 0
 
-    df_tpsl = pd.DataFrame()
-
-    for row in df.itertuples():  # Итерация по рандже барам
-        if row[low_idx] <= row[mvc_idx] <= row[high_idx]:  # Если макс объем в баре (защита от ошибочных баров)
-            df_tmp = pd.DataFrame({'field': [row[low_idx], row[high_idx]]})
-            if row[close_idx] == row[high_idx]:  # Если бар на повышение
-                val = df_tmp['field'].quantile(edge_only)
-                if row[mvc_idx] <= val:
-                    continue
-            elif row[close_idx] == row[low_idx]:  # Если бар на понижение
-                val = df_tmp['field'].quantile(1 - edge_only)
-                if row[mvc_idx] >= val:
-                    continue
-        df_tpsl[row.index, '<TP_SL>'] = 0
-    return df_tpsl
+    df['<TP_SL>'] = df.apply(lambda x: replace_cell(x['<HIGH>'],
+                                                    x['<LOW>'],
+                                                    x['<CLOSE>'],
+                                                    x['<MAX_VOLUME_PRICE>'],
+                                                    x['<TP_SL>']),
+                             axis=1)
+    return df
 
 
 if __name__ == "__main__":
     symbol = 'RTS'
-    edge_only = 0.0  # Значение установить в 0 если не нужны перцентили ТП/СЛ
+    edge_only = 0.25  # Значение установить в 0 если не нужны перцентили ТП/СЛ
+    ema_period = 18
     # Формат файла
     """
     <DATE>,<TIME>,<OPEN>,<HIGH>,<LOW>,<CLOSE>,<VOL>,<MAX_VOLUME_PRICE>,<MAX_VOLUME_CLUSTER>,<TP_SL>
@@ -77,23 +84,42 @@ if __name__ == "__main__":
     pd.set_option('display.max_columns', None)  # Сброс ограничений на число столбцов
 
     # Обработка DF
+    df['<EMA>'] = df['<CLOSE>'].ewm(span=ema_period, adjust=False).mean()
     df['<TIME>'] = df.apply(lambda x: zero_hour(x['<TIME>']), axis=1)  # Преобразуем столбец <TIME>, где нужно добавив 0 перед часом
     df['<DATE_TIME>'] = df['<DATE>'].astype(str) + ' ' + df['<TIME>'].astype(str)  # Создаем новый столбец <DATE_TIME> слиянием столбцов <DATE> и <TIME>
     df = df.set_index(pd.DatetimeIndex(df['<DATE_TIME>']))  # Меняем индекс и делаем его типом datetime
-    df.drop(labels=['<DATE_TIME>', '<DATE>', '<TIME>', '<VOL>'], axis=1, inplace=True)  # Удаляем ненужные колонки. axis=1 означает, что отбрасываем колонку а не индекс
+    df.drop(labels=['<DATE_TIME>', '<DATE>', '<TIME>'], axis=1, inplace=True)  # Удаляем ненужные колонки. axis=1 означает, что отбрасываем колонку а не индекс
 
     if edge_only != 0:
         df_tpsl = edge_prepare(df, edge_only)
 
+    # print(df)
 
-    print(df)
+    df_new = df.loc[df['<TP_SL>'] != 0]  # Выбираются только строки удовлетворяющие перцентилю MVC
+
+    df_up = df_new.loc[df_new['<LOW>'] >= df_new['<EMA>']]  # EMA
+    df_up = df_up.loc[df_up['<CLOSE>'] == df_up['<HIGH>']]  # Бар на повышение
+
+    df_down = df_new.loc[df_new['<HIGH>'] <= df_new['<EMA>']]  # EMA
+    df_down = df_down.loc[df_down['<CLOSE>'] <= df_down['<LOW>']]  # Бар на понижение
+    # print(df_new)
+
+    print('\nВыше ЕМА')
+    print(df_up['<TP_SL>'].value_counts())
+    print(df_up['<TP_SL>'].value_counts(normalize=True))
+
+    print('\nНиже EMA')
+    print(df_down['<TP_SL>'].value_counts())
+    print(df_down['<TP_SL>'].value_counts(normalize=True))
 
     # Построение графика
-    ax1, ax2 = fplt.create_plot(symbol, rows=2)  # Cоздаем окна
+    ax1, ax2, ax3 = fplt.create_plot(symbol, rows=3)  # Cоздаем окна
     plot_tpsl(df, ax2)  #
+    plot_vol(df, ax3)  #
     plot_candlestick(df, ax1)  # рисуем свечной график в основном окне
-    plot_ema(df, ax1)
+    plot_ema(df, ax1, ema_period)
 
     ax2.setXLink(ax1)  # Для синхронизации осей Х
+    ax3.setXLink(ax1)  # Для синхронизации осей Х
 
     fplt.show()
